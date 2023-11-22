ad2721b536c343e45d55d9c5bd9d072f66b75d25
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-11-22 18:24:28 -0300

    Update20231122182437

--- js/scripts.js
+++ js/scripts.js
@@ -19,31 +19,40 @@ closeBtn.addEventListener("click", () => {
 	menu.classList.remove('active');
 });
 
+//  <!-- Google tag (gtag.js) -->
+//  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PHZT96Z5RF"></script>
+    //  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-//  <!-- Google Tag Manager -->
-//  <script>
-    //(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        //new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        //j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        //'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        //})(window,document,'script','dataLayer','GTM-PCWRWW3');
-//  </script>
+    gtag('config', 'G-PHZT96Z5RF');
+    //  </script>
+
+// <!-- Google Tag Manager -->
+// <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M7CHRLNL');
+// </script>
 //  <!-- End Google Tag Manager -->
 
 //<!-- GetButton.io widget -->
 //<script type="text/javascript">
-    //(function () {
-        //var options = {
-            //whatsapp: "+55(13) 98126-9443", // WhatsApp number
-            //call_to_action: "Envia-nos uma mensagem", // Call to action
-            //position: "right", // Position may be 'right' or 'left'
-            //pre_filled_message: "Olá!  venho do seu site, eu gostaria de fazer um orçamento!!!", // WhatsApp pre-filled message
-        //};
-        //var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        //var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        //s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        //var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    //})();
+    (function () {
+        var options = {
+            whatsapp: "+55(13) 98126-9443", // WhatsApp number
+            call_to_action: "Envia-nos uma mensagem", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+            pre_filled_message: "Olá!  venho do seu site CARRETOS PRAIAGRANDE, eu gostaria de fazer um orçamento!!!", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
 //</script>
 //<!-- /GetButton.io widget -->
 
