6a07c8fa7ce27c0794b5b3da1a4924db463d7f8f
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-12-14 11:46:00 -0300

    Update20231214114535

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,7 @@ closeBtn.addEventListener("click", () => {
             whatsapp: "+55(13) 98126-9443", // WhatsApp number
             call_to_action: "Envia-nos uma mensagem", // Call to action
             position: "right", // Position may be 'right' or 'left'
-            pre_filled_message: "Olá!  venho do seu site CARRETOS PRAIAGRANDE, eu gostaria de fazer um orçamento!!!", // WhatsApp pre-filled message
+            pre_filled_message: "Olá! Venho através do seu Site Carretos Praia Grande!!!", // WhatsApp pre-filled message
         };
         var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
